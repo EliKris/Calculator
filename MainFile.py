b87deab37a39ef24b0afd04022824a41cd98f6a8
EliKris <45640615+EliKris@users.noreply.github.com>
2019-01-20 22:01:41 +0100

    The End
    So now I have finished my Project, that was making a calculator in Python.

--- MainFile.py
+++ MainFile.py
@@ -1,11 +1,22 @@
 #The start of the program
 #Trying to make the user input fetuare for the calculator
-#The print functions are mostly made for telling the user what to do.
 print("Type in the first nummber and press enter")
-FI = int(input("Enter first nummber here:")) #This is the first input for the calculator
+FI = int(input("Enter first nummber here:"))
 print("Type in the second nummber and press enter")
-SI = int(input("Enter second nummber here:")) #This is the second input for the calculator
+SI = int(input("Enter second nummber here:"))
 print("Type in the operator and press enter (Exampels, +, -, *, /)")
-V = (input("Enter The Operator here:")) #This is the input for the operator, things like +, -, and * 
-ASVR = FI + V + SI   #This is the part where we take all the inputs and put them together and get the answer
-print(ASVR) #Now we have the answer and can print it out to the user
+V = (input("Enter The Operator here:"))
+
+if V == "+":
+    print(FI + SI)
+else:
+    if V == "-":
+        print(FI - SI)
+    else:
+        if V == "*":
+            print(FI * SI)
+        else:
+            if V == "/":
+                print(FI / SI)
+            else:
+                print("Subscribe to pewdiepie")
